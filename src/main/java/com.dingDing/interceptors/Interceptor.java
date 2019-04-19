d9335a40ab9a2a2f65b6e5aa8519d7b01c21018f
soul2323 <soul_zhanglu@163.com>
2019-04-19 14:44:38 +0800

    123

---
+++ src/main/java/com.dingDing/interceptors/Interceptor.java
@@ -0,0 +1,15 @@
+package com.dingDing.interceptors;
+
+public class Interceptor {
+
+    public static void main(String t[]){
+        boolean s=false;
+        if(!s){
+            System.out.print("true");
+        }else{
+
+            System.out.print("false");
+        }
+    }
+
+}
